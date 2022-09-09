b547e5c59213859f4567c271d3d1cb09e37b7d57
Mahmoud Qandeel <mahmoud.waleed2015@gmail.com>
2022-09-09 21:18:26 +0300

    first commit

---
+++ ___FILEBASENAME___.swift
@@ -0,0 +1,40 @@
+import Foundation
+import UIKit
+
+class ___FILEBASENAME___: BaseController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupView()
+        self.localized()
+        self.setupData()
+        self.fetchData()
+    }
+    
+    
+}
+
+extension ___FILEBASENAME___ {
+    
+    func setupView() {
+        
+    }
+    
+    func localized() {
+        
+    }
+    
+    func setupData() {
+        
+    }
+    
+    func fetchData() {
+        
+    }
+}
+
+extension ___FILEBASENAME___ {
+    
+    
+}
+
